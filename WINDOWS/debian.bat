50c9d7e085d2151781143beba6c3e0b283ef940b
Franco Corbelli <77727889+fcorbelli@users.noreply.github.com>
2022-09-19 11:19:39 +0200

    Add files via upload

---
+++ WINDOWS/debian.bat
@@ -0,0 +1,6 @@
+copy zpaqfranz.cpp zpaqfranz-debian.cpp
+sed -i "/DEBIANSTART/,/\/\/\/DEBIANEND/d"  zpaqfranz-debian.cpp
+sed -i "s/\/\/\/char extract_test1/char extract_test1/g" zpaqfranz-debian.cpp
+
+sed -i "s/(\" __DATE__ \")/(\" Debian \")/g" zpaqfranz-debian.cpp
+

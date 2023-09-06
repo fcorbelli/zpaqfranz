e7f645008067a910ec9d6b3e6ac24bf240257356
Franco Corbelli <77727889+fcorbelli@users.noreply.github.com>
2023-09-06 14:53:39 +0200

    Add files via upload

--- ZSFX/zsfx.cpp
+++ ZSFX/zsfx.cpp
@@ -7,31 +7,35 @@ Embedded in zpaqfranz for Windows
 Written by Franco Corbelli
 https://github.com/fcorbelli
 
-# Those are the Windows 32/64 SFX modules for zpaqfranz  
 
-### Released by Franco Corbelli, with MIT License
 
-**zfs.cpp** use two files: **libzpaq.cpp** and **libzpaq.h**,  
+=======================================================
+Those are the Windows 32/64 SFX modules for zpaqfranz  
+
+Released by Franco Corbelli, with MIT License
+
+zsfx.cpp use two files: libzpaq.cpp and libzpaq.h,  
 with compatible licenses (MIT and public domain)  
 
-### libzpaq.cpp 
+libzpaq.cpp 
 use libdivsufsort.c for divsufsort 2.00, included within  
 (C) 2003-2008 Yuta Mori, all rights reserved.  
-It is released under the **MIT license** as described in the comments  
+It is released under the MIT license as described in the comments  
 at the beginning of that section.  
 
 Some of the code for AES is from libtomcrypt 1.17 by Tom St. Denis  
-and is **public domain**.  
+and is public domain.  
 
-The Salsa20/8 code for Scrypt is by D. Bernstein and is **public domain**.  
+The Salsa20/8 code for Scrypt is by D. Bernstein and is public domain.  
 
-All of the remaining software is provided as-is, with no warranty.  
-by Matt Mahoney, release this software into the **public domain**.  
+All of the remaining software is provided as-is, with no warranty  
+by Matt Mahoney, who release this software into the public domain.  
 
-### libzpaq.h 
+libzpaq.h 
 is provided as-is, with no warranty, by  
-Matt Mahoney, release this software into the **public domain**     
+Matt Mahoney, who release this software into the public domain     
 This applies worldwide.
+=======================================================
 
 
 

6f9639f87aed40bcde775b7da6c0cd4b2f2f9548
Franco Corbelli <77727889+fcorbelli@users.noreply.github.com>
2023-09-06 11:15:25 +0200

    Delete WINDOWS directory

--- WINDOWS/err.bat
+++
@@ -1,6 +0,0 @@
-@echo off  
-del .\zpaqfranz.exe
-del z:\1.zpaq
-del z:\commit.zpaq
-del z:\normale.zpaq
-g++ -DANCIENT zpaqfranz.cpp -o zpaqfranz -Wunused-parameter -Wall -Wextra -pedantic  >1.txt 2>&1
--- WINDOWS/o3.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-del .\zpaqfranz.exe
-g++ -O3 -DHWBLAKE3 blake3_windows_gnu.s zpaqfranz.cpp -o zpaqfranz -pthread -static
-strip zpaqfranz.exe
